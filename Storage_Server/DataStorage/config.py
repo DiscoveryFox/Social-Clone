03d8480e371cc8c7761a4bc04eda0428f1bdd5fc
discovery-bit <61625792+discovery-bit@users.noreply.github.com>
2023-06-20 23:44:50 +0200

    updated config

--- Storage_Server/DataStorage/config.py
+++ Storage_Server/DataStorage/config.py
@@ -1,4 +1,3 @@
-DATABASE_PATH: str = r'C:\Users\Flinn\Documents\Social-Clone\Storage ' \
-                     r'Server\DataStorage\tests\test.db'
+DATABASE_PATH: str = r'C:\Users\Flinn\Documents\Social-Clone\storage\test.db'
 DATABASE_TIMEOUT: int = 30
 STORAGE_PATH: str = r'C:\Users\Flinn\Documents\Social-Clone\storage'
